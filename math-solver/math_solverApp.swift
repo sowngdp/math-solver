735d01a0e837c6be96d07225530107326f2d8bd6
sowngdp <denis.santaris@gmail.com>
2025-09-13 21:35:48 +0700

    Initial Commit

---
+++ math-solver/math_solverApp.swift
@@ -0,0 +1,17 @@
+//
+//  math_solverApp.swift
+//  math-solver
+//
+//  Created by SownFrenky on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct math_solverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
